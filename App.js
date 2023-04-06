226d838191cb0a64a34b06534530fc7b9dfe0e32
Wolf17zzz <arthurpimentelparreira13@gmail.com>
2023-04-05 22:31:18 -0300

    Primeiro código

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}>Olá Mundo!</Text>
+      
+      <Text>Meu nome é Arthur e esse é meu primeiro app em react!</Text>
     </View>
   );
 }
@@ -14,7 +15,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'start',
   },
+
+  titulo:{
+    backgroundColor: "#9F83F9",
+    padding: 10,
+    borderRadius: 5
+  }
 });

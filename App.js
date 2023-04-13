8577d69967d4031d08389ebcd6e22e2359693321
Wolf17zzz <arthurpimentelparreira13@gmail.com>
2023-04-12 22:20:52 -0300

    Criando Páginas 1

--- App.js
+++ App.js
@@ -1,4 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+import Home from './src/pages/Home';
+import Home from './src/pages/Formacao';
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home}/>
+
+        <Stack.Screen name="Formacao" component={Formacao}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Olá Mundo!</Text>
+      
+      <Text>Meu nome é Arthur e esse é meu primeiro app em react!</Text>
+    </View>
+  );
+}
